85421e6d32967f782926093c80eae5da2bbccf1d
Gideon Lingle <gideon.lingle1@gmail.com>
2019-05-26 03:01:00 -0500

    Update setgame.lua

--- commands/setgame.lua
+++ commands/setgame.lua
@@ -7,7 +7,9 @@ return {
     local mArray = string.split(message.content, " ")
     local args = table.slice(mArray, 2)
     
-    print(table.concat(args))
+    local joined_args = table.concat(args, " ")
+    
+    local resplitted = string.split(joined_args[1])
 --     bot:setGame({name = args, type = 3})
   end
 }
