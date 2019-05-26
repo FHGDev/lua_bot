0933cc225e454d61687596755ff4763eb45b5524
Gideon Lingle <gideon.lingle1@gmail.com>
2019-05-26 02:22:50 -0500

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -9,8 +9,25 @@ bot:on('ready', function()
 end)
 
 bot:on('messageCreate', function(message)
-  if (bot._commands[message.content]) then
-    bot._commands[message.content].run(bot, message)
+  if (bot.message.content]) then
+    botlocal discordia = require "discordia"
+local os = require "os"
+local token = os.getenv("token")
+local bot = discordia.Client()
+bot._commands = require "./commands.lua"
+
+bot:on('ready', function()
+  print("Logged in as", bot.user.username)
+end)
+
+bot:on('messageCreate', function(message)
+  if (bot._commands[message.content:match('^%w+')]) then
+    bot._commands[message.content:match('^%w+')].run(bot, message)
+  end
+end)
+
+bot:run("Bot "..token)
+._commands[message.content].run(bot, message)
   end
 end)
 
