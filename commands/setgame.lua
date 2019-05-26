a763b7e248ce39d19534611f20e38c42613ee1b8
Gideon Lingle <gideon.lingle1@gmail.com>
2019-05-26 03:59:50 -0500

    Update setgame.lua

--- commands/setgame.lua
+++ commands/setgame.lua
@@ -1,5 +1,6 @@
 local discord = require('discordia')
 local valid_presences = {["Watching"] = 3, ["Listening"] = 2, ["Streaming"] = 1, ["Playing"] = 0}
+local numbered_presences = { 0 = "Playing", 1 = "Streaming", 2 = "Listening", 3 = "Watching" }
 
 discord.extensions()
 
@@ -10,8 +11,10 @@ return {
     
     local resplitted = table.slice(args, 2)
     local game = table.concat(resplitted, " ")
-    local type = tonumber(args[1])
+    local type = args[1]
     print(type.." "..game)
+
+    if check(type, valid_presences) and not check(type, numbered_presences) then type == numbered_presences[type] else return end
     
     local s, e = pcall(function()
         bot:setGame({name=game, type=type})
@@ -24,3 +27,9 @@ return {
     if (e) then print(e) end
   end
 }
+
+function check(val,tab)
+  for _,v in next, tab do
+    if val == v then return true else return false end;  
+  end
+end
