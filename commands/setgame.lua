eefe8e627a05f2190fc0bcec2386ad9a68834b14
Gideon Lingle <gideon.lingle1@gmail.com>
2019-05-26 03:17:08 -0500

    Update setgame.lua

--- commands/setgame.lua
+++ commands/setgame.lua
@@ -7,7 +7,7 @@ return {
     local mArray = string.split(message.content, " ")
     local args = table.slice(mArray, 2)
     
-    local resplitted = table.slice(args, 1)
+    local resplitted = table.slice(args, 2)
     
     for _,v in next, resplitted do
       if (type(v) ~= nil) then 
