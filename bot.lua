6a9911642b1a570d9978365819783a79969ba63c
Gideon Lingle <gideon.lingle1@gmail.com>
2019-05-26 01:49:36 -0500

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -1,9 +1,10 @@
 local discordia = require "discordia"
 local os = require "os"
+print(os.getenv("token"))
 local bot = discordia.Client()
 
 bot:on('ready', function()
   print("Logged in as", bot.user.username)
 end)
 
-bot:run("Bot",os.getenv("token"))
+bot:run()
