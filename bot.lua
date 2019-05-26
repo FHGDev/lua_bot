430e9827eeea5aa15ee5e6819a276fad49f59ccb
Gideon Lingle <gideon.lingle1@gmail.com>
2019-05-26 01:56:07 -0500

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -2,13 +2,16 @@ local discordia = require "discordia"
 local os = require "os"
 local token = os.getenv("token")
 local bot = discordia.Client()
+bot._commands = require "./commands.lua"
 
 bot:on('ready', function()
   print("Logged in as", bot.user.username)
 end)
 
 bot:on('messageCreate', function(message)
-  print(message.content)
+  if (bot._commands[message.content]) then
+    bot._commands[message.content].run(bot, message)
+  end
 end)
 
 bot:run("Bot "..token)
