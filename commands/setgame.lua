b59fd4dbcca65ce58717ef161266d8d14f4054da
Gideon Lingle <gideon.lingle1@gmail.com>
2019-05-26 02:50:27 -0500

    Update setgame.lua

--- commands/setgame.lua
+++ commands/setgame.lua
@@ -4,8 +4,8 @@ discord.extensions()
 
 return {
   run = function(bot, message)
-    const args = message.content:split(" ")
-    const type = args:slice(#args)
+    local args = message.content:split(" ")
+    local type = args:slice(#args)
     
     print(args)
 --     bot:setGame({name = args, type = 3})
