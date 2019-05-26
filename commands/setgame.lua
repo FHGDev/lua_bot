485221875f00a9f2de1b87925e04587e882e8d8b
Gideon Lingle <gideon.lingle1@gmail.com>
2019-05-26 02:58:23 -0500

    Update setgame.lua

--- commands/setgame.lua
+++ commands/setgame.lua
@@ -5,7 +5,7 @@ discord.extensions()
 return {
   run = function(bot, message)
     local mArray = string.split(message.content, " ")
-    local args = table.slice(mArray, 1)
+    local args = table.slice(mArray, 2)
     
     print(table.concat(args))
 --     bot:setGame({name = args, type = 3})
