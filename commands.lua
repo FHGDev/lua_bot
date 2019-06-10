08c4150c995cc91417ad92e0f55f3965ad3c12d9
Gideon Lingle <gideon.lingle1@gmail.com>
2019-06-09 20:27:35 -0500

    Update commands.lua

--- commands.lua
+++ commands.lua
@@ -1,5 +1,6 @@
 return {
   ["ping"] = require('./commands/ping.lua'),
   ["say"] = require("./commands/say.lua"),
-  ["sgame"] = require('./commands/setgame.lua')
+  ["sgame"] = require('./commands/setgame.lua'),
+  ["eval"] = require('./commands/eval.lua')
 }
