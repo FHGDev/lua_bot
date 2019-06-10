ab563f5f1cbf495a6b3b8e058d30c80087faf907
Gideon Lingle <gideon.lingle1@gmail.com>
2019-06-09 20:25:58 -0500

    Create eval.lua

---
+++ commands/eval.lua
@@ -0,0 +1,21 @@
+local exports = {}
+
+-- code here
+
+exports.run = function(bot,message)
+  local args = message.content:sub(6)
+  if not args then message.channel:send("You must give me some code to execute."); end
+    if message.author ~= message.client.owner then return end
+
+    local fn, syntaxError = load(arg)
+    if not fn then return message.channel:send(code(syntaxError)) end
+
+    local success, runtimeError = pcall(fn) -- run the code
+    if not success then return message.channel:send(code(runtimeError)) end -- handle runtime errors
+end
+
+return exports
+
+function code(str)
+  return string.format('```lua\n%s```', str)
+end
